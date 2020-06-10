20c9e1764b8f283ec7f18aebefc14fbea567571a
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-06-10 16:23:02 +0300

    Added continentmanager

---
+++ Assets/Scripts/ContinentManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinentManager : MonoBehaviour
+{
+    public static ContinentManager continentManager;
+
+    private void Awake()
+    {
+        continentManager = this;
+    }
+
+
+    public GameObject afrikka;
+}
