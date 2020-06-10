c3705f9f53db7fb09251098328f4d922fdae3f5c
Antti Heimonen <anttiheimonen@hotmail.com>
2020-06-10 14:40:09 +0300

    Added Continent script

---
+++ Assets/Scripts/Continent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Continent : MonoBehaviour
+{
+
+
+    public string continentName;
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        // Debug.Log("Mouse is over GameObject.");
+        if (Input.GetMouseButtonDown(0))
+        {
+            gm.SelectContinent("Afrikka");
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,19 @@ public class GameMaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    public void SelectContinent(string continent)
+    {
+        Debug.Log(continent);
     }
 }
