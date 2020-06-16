79f96f0520eebaf29fcc8a5f20685c987c9b195e
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-06-16 18:18:05 +0300

    Remade mouseovering

--- Assets/Scripts/CountryController.cs
+++ Assets/Scripts/CountryController.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class CountryController : MonoBehaviour
 {
     private Vector3 mouseOverScale = new Vector3(1.2f, 1.2f, 1.2f);
+    private bool mouseOver;
+    private Color originalColor;
+    private bool answered;
 
     private void OnMouseDown()
     {
         string country = gameObject.name;
         // Debug.Log("Valittu maa " + country);
         GameManager.Instance.SelectCountry(country);
+        originalColor = gameObject.GetComponent<SpriteRenderer>().color;
         // Debug.Log("Clicked " + gameObject.name);
         // TODO: fix at some point
     }
@@ -19,18 +23,27 @@ public class CountryController : MonoBehaviour
 
     private void OnMouseOver()
     {
-        // Debug.Log("JEE");
-        gameObject.transform.localScale = mouseOverScale;
-        // ChangeColorToYellow();
-        gameObject.GetComponent<SpriteRenderer>().color = gameObject.GetComponent<SpriteRenderer>().color
-                                                        + new Color(20, 20, 20, 0);
+        if (!answered)
+        {
+            if (!mouseOver)
+            {
+                mouseOver = true;
+                originalColor = gameObject.GetComponent<SpriteRenderer>().color;
+            }
+            gameObject.transform.localScale = mouseOverScale;
+            gameObject.GetComponent<SpriteRenderer>().color
+                = gameObject.GetComponent<SpriteRenderer>().color
+                * new Color(0.96f, 0.96f, 0.96f, 1f);
+        }
     }
 
 
     private void OnMouseExit()
     {
+        mouseOver = false;
         gameObject.transform.localScale = Vector3.one;
-        ChangeColorToWhite();
+        // originalColor = gameObject.GetComponent<SpriteRenderer>().color;
+        ChangeColorTo(originalColor);
     }
 
 
@@ -71,8 +84,16 @@ public class CountryController : MonoBehaviour
     }
 
 
+    private void ChangeColorTo(Color color)
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = color;
+    }
+
+
     private void WrongCountry()
     {
+        answered = true;
+        // originalColor = Color.red;
         ChangeColorToRed();
         UIManager.instance.UpdateAnswerFeedBack("Väärin");
         GameManager.Instance.GetComponent<SoundController>().PlaySound(SoundManager.instance.wrongAnswer);
@@ -82,6 +103,8 @@ public class CountryController : MonoBehaviour
 
     private void RightCountry()
     {
+        answered = true;
+        // originalColor = Color.green;
         ChangeColorToGreen();
         UIManager.instance.UpdateAnswerFeedBack("Oikein!");
         GameManager.Instance.GetComponent<SoundController>().PlaySound(SoundManager.instance.rightAnswer);
